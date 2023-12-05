6dcffc114ef9751dc6a865de58e28c34fb8098d3
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-12-05 15:51:50 -0500

    Cleared values when contact me form is submitted

--- script/script.js
+++ script/script.js
@@ -2,6 +2,27 @@ let menu = document.getElementById('menu-bars');
 let header = document.querySelector('header');
 let cursor1 = document.querySelector('.cursor-1');
 let cursor2 = document.querySelector('.cursor-2');
+const submitButton = document.getElementById('submit-form');
+
+const formSubmitHandler = (e) => {
+    e.preventDefault();
+    e.target.form[0].form[0].value = '';
+    e.target.form[0].form[1].value = '';
+    e.target.form[0].form[2].value = '';
+    e.target.form[0].form[3].value = '';
+};
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+};
+
+window.onmousemove = (e) => {
+    cursor1.style.top = e.pageY + 'px';
+    cursor1.style.left = e.pageX + 'px';
+    cursor2.style.top = e.pageY + 'px';
+    cursor2.style.left = e.pageX + 'px';
+};
 
 document.querySelectorAll('a').forEach(links => {
     links.onmouseenter = () => {
@@ -18,16 +39,6 @@ document.querySelectorAll('a').forEach(links => {
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
-}
+};
 
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    header.classList.remove('active');
-}
-
-window.onmousemove = (e) => {
-    cursor1.style.top = e.pageY + 'px';
-    cursor1.style.left = e.pageX + 'px';
-    cursor2.style.top = e.pageY + 'px';
-    cursor2.style.left = e.pageX + 'px';
-}
+submitButton.addEventListener('click', formSubmitHandler)
